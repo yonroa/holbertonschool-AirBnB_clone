58a452f8970b7c26743829a8ab32d9771ad8e5d7
Viktor <viktorkrilldj@gmail.com>
2022-06-30 21:14:58 -0500

    base model 1

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,8 @@ the base for all other classes
 
 
 import uuid
+import datetime
+
 
 
 class BaseModel:
@@ -14,3 +16,22 @@ class BaseModel:
 
     def __init__(self, id=uuid.uuid4()):
         self.id = str(id)
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        
+        self.updated_at = datetime.datetime.now()
+    
+    def to_dict(self):
+
+        new_dict = self.__dict__.copy()
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
+    
