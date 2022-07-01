942ff4157f4a0ad8f6866bac458db7a6879e70cf
yonroa <yonanderson11@gmail.com>
2022-06-30 21:25:18 -0500

    Document update

--- models/base_model.py
+++ models/base_model.py
@@ -8,28 +8,28 @@ import uuid
 import datetime
 
 
-
 class BaseModel:
-    """Class that defines all common attributes/methods 
+    """Class that defines all common attributes/methods
     for other classes
     """
 
     def __init__(self, id=uuid.uuid4()):
+        """Initialites a base model"""
         self.id = str(id)
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-
+        """Returns a nice print of the base model"""
         return "[{}] ({}) {}".format(
             type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        
+        """Saves the date of the update of the class"""
         self.updated_at = datetime.datetime.now()
-    
-    def to_dict(self):
 
+    def to_dict(self):
+        """Returns a new dict of the class"""
         new_dict = self.__dict__.copy()
         new_dict['created_at'] = self.created_at.isoformat()
         new_dict['updated_at'] = self.updated_at.isoformat()
