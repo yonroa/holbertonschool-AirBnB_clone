5ecf5dc97fb15c46b2dcb89b23d1e9140879931a
Viktor <viktorkrilldj@gmail.com>
2022-06-30 21:42:24 -0500

    strptime

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,22 @@ class BaseModel:
     for other classes
     """
 
-    def __init__(self, id=uuid.uuid4()):
+    def __init__(self, *args, **kwargs):
         """Initialites a base model"""
-        self.id = str(id)
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    self.created_at = datetime.datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == 'updated_at':
+                    self.updated_at = datetime.datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Returns a nice print of the base model"""
