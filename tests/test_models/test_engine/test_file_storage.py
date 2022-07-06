24c4ed941607652ee191193a1ed6eef6d4a3f386
yonroa <yonanderson11@gmail.com>
2022-07-06 17:45:19 -0500

    Update test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -124,6 +124,22 @@ class TestFileStorage(unittest.TestCase):
         """Tests all_multiple() method for Review."""
         self.prueba_all("Review")
 
+    def test_5_all_no_args(self):
+        """Tests all() with no arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            FileStorage.all()
+        msg = "all() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_5_all_excess_args(self):
+        """Tests all() with too many arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            FileStorage.all(self, 98)
+        msg = "all() takes 1 positional argument but 2 were given"
+        self.assertEqual(str(e.exception), msg)
+
     def Prueba_new(self, line):
         """"""
         self.resetStorage()
@@ -160,6 +176,23 @@ class TestFileStorage(unittest.TestCase):
     def test_new_review(self):
         """Tests new() method for Review."""
         self.Prueba_new("Review")
+
+    def test_5_new_no_args(self):
+        """Tests new() with no arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            storage.new()
+        msg = "new() missing 1 required positional argument: 'obj'"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_5_new_excess_args(self):
+        """Tests new() with too many arguments."""
+        self.resetStorage()
+        b = BaseModel()
+        with self.assertRaises(TypeError) as e:
+            storage.new(b, 98)
+        msg = "new() takes 2 positional arguments but 3 were given"
+        self.assertEqual(str(e.exception), msg)
 
     def prueba_save(self, line):
         """"""
@@ -204,6 +237,22 @@ class TestFileStorage(unittest.TestCase):
         """Tests save() method for Review."""
         self.prueba_save("Review")
 
+    def test_5_save_no_args(self):
+        """Tests save() with no arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            FileStorage.save()
+        msg = "save() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_5_save_excess_args(self):
+        """Tests save() with too many arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            FileStorage.save(self, 98)
+        msg = "save() takes 1 positional argument but 2 were given"
+        self.assertEqual(str(e.exception), msg)
+
     def prueba_reload(self, line):
         """"""
         self.resetStorage()
@@ -242,6 +291,22 @@ class TestFileStorage(unittest.TestCase):
     def test_reload_review(self):
         """Tests reload() method for Review."""
         self.prueba_reload("Review")
+
+    def test_reload_no_args(self):
+        """Tests reload() with no arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            FileStorage.reload()
+        msg = "reload() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_reload_excess_args(self):
+        """Tests reload() with too many arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            FileStorage.reload(self, 98)
+        msg = "reload() takes 1 positional argument but 2 were given"
+        self.assertEqual(str(e.exception), msg)
 
 
 if __name__ == "__main__":
