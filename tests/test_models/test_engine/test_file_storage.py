59db277fcfd48de5547650b6edba8e4aa3fdb52d
yonroa <yonanderson11@gmail.com>
2022-07-06 17:53:18 -0500

    Update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -41,6 +41,22 @@ class TestFileStorage(unittest.TestCase):
         """"""
         self.assertEqual(type(storage).__name__, "FileStorage")
 
+    def test_init_no_args(self):
+        """Tests __init__ with no arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            FileStorage.__init__()
+        msg = "descriptor '__init__' of 'object' object needs an argument"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_init_many_args(self):
+        """Tests __init__ with many arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            b = FileStorage(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+        msg = "FileStorage() takes no arguments"
+        self.assertEqual(str(e.exception), msg)
+
     def test_attr(self):
         """"""
         self.resetStorage()
